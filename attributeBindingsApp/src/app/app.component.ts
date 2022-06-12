953cd1032b60e99aaa683de131cf42dc18dcf848
hosein erfani <skyler.university@gmail.com>
2022-06-12 13:22:10 +0430

    switch cases

--- attributeBindingsApp/src/app/app.component.ts
+++ attributeBindingsApp/src/app/app.component.ts
@@ -15,6 +15,7 @@ export class AppComponent implements OnInit {
   styles = {}
   classes = {}
 
+  switchKeyWord:string='a';
 
 
   usersObj: any = [
---
+++ attributeBindingsApp/src/app/switch-case4/switch-case4.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-switch-case4',
+  templateUrl: './switch-case4.component.html',
+  styleUrls: ['./switch-case4.component.css']
+})
+export class SwitchCase4Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
