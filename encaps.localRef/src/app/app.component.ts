ff92234276f380b7dbd581736d8ea6478e9c1e6e
hosein erfani <skyler.university@gmail.com>
2022-06-14 18:01:31 +0430

    view child and view children

--- encaps.localRef/src/app/app.component.ts
+++ encaps.localRef/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, OnInit, QueryList, ViewChild, ViewChildren, ViewEncapsulation } from '@angular/core';
+import { IncComponent } from './inc/inc.component';
 
 @Component({
   selector: 'app-root',
@@ -6,11 +7,21 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./app.component.css'],
   encapsulation:ViewEncapsulation.None,
 })
-export class AppComponent {
-  title = 'encaps.localRef';
+export class AppComponent{
+
+
+
+  @ViewChild(IncComponent,{static:true,read:IncComponent}) incCom?:IncComponent;
+  @ViewChildren(IncComponent,{read:IncComponent}) incComs?:QueryList<IncComponent>;
 
   public showAlert(inputElem:HTMLInputElement){
     console.log(inputElem.value);
     inputElem.value='';
+  }
+  public increament2(){
+    // this.incCom?.increament();
+    this.incComs?.forEach(inCom=>{
+      inCom.increament();
+     })
   }
 }
--- encaps.localRef/src/app/inc/inc.component.ts
+++ encaps.localRef/src/app/inc/inc.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-inc',
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IncComponent implements OnInit {
 
-  constructor() { }
+  public  count:number=0;
+ public  alertClass="";
+
+
+  @ViewChild('testText',{static:true,read:ElementRef}) testText!:ElementRef;
+
+  constructor( private renderer:Renderer2) {
+    
+
+    this.alertClassHandler()
+   } 
+
+
+  public increament(){
+    this.count++;
+    this.alertClassHandler()
+  }
+  public decreament(){
+    this.count--;
+    this.alertClassHandler()
+  }
+
 
   ngOnInit(): void {
+    // this.testText.nativeElement.innerText=332;
+    // console.log(this.testText);
+    this.renderer.setStyle(this.testText.nativeElement,'color','purple')
+
+  }
+  
+  public alertClassHandler(){
+    this.alertClass="alert "+((this.count>0)?"alert-info":"alert-danger");
   }
 
 }
